21be58745dcb77177e56326fc1de1a9b162b23cc
saint-vitus <vit.mikula101@alu.ulpgc.es>
2025-03-11 11:48:23 +0000

    Add some methods

--- src/main/java/software/ulpgc/edp/Dictionary.java
+++ src/main/java/software/ulpgc/edp/Dictionary.java
@@ -1,10 +1,12 @@
 package software.ulpgc.edp;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 
-public class Dictionary<K, V> {
-    private static class Entry<K, V> {
+public class Dictionary<K, V>  implements Iterable<Dictionary.Entry<K, V>>{
+    protected static class Entry<K, V> {
         K key;
         V value;
         Entry<K, V> next;
@@ -14,9 +16,14 @@ public class Dictionary<K, V> {
             this.value = value;
             this.next = null;
         }
+
+        @Override
+        public String toString() {
+            return key + ": " + value;
+        }
     }
 
-    private Entry<K, V>[] table;
+    private final Entry<K, V>[] table;
     private int size;
     private static final int INITIAL_CAPACITY = 16;
 
@@ -31,6 +38,11 @@ public class Dictionary<K, V> {
         if (o == null || getClass() != o.getClass()) return false;
         Dictionary<?, ?> that = (Dictionary<?, ?>) o;
         return size == that.size && Objects.deepEquals(table, that.table);
+    }
+
+    @Override
+    public Iterator<Entry<K, V>> iterator() {
+        return new DictionaryIterator();
     }
 
     @Override
@@ -65,7 +77,71 @@ public class Dictionary<K, V> {
         size++;
     }
 
-    public void remove() {
-
+    public V get(K key) {
+        int index = hash(key);
+        for (Entry<K, V> e = table[index]; e != null; e = e.next) {
+            if (e.key != null && e.key.equals(key)) {
+                return e.value;
+            }
+        }
+        return null;
     }
+
+    public void remove(K key) {
+        int index = hash(key);
+        if(table[index] == null) return;
+        else {
+            Entry<K, V> current = table[index];
+            while(current.next != null) {
+                if(current.next.key.equals(key)) {
+                    current.next = current.next.next;
+                }
+                current = current.next;
+            }
+        }
+        size--;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    private class DictionaryIterator implements Iterator<Entry<K, V>> {
+        private int index = 0;
+        private Entry<K, V> current = null;
+
+        public DictionaryIterator() {
+            advanceToNextBucket();
+        }
+
+        private void advanceToNextBucket() {
+            while (index < table.length && table[index] == null) {
+                index++;
+            }
+            if (index < table.length) {
+                current = table[index];
+            } else {
+                current = null;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Entry<K, V> next() {
+            if(!hasNext()) throw new NoSuchElementException();
+            Entry<K, V> entryToReturn = current;
+            if(current.next != null) {
+                current = current.next;
+            } else {
+                index++;
+                advanceToNextBucket();
+            }
+            return entryToReturn;
+        }
+    }
+
 }
--- src/main/java/software/ulpgc/edp/Main.java
+++ src/main/java/software/ulpgc/edp/Main.java
@@ -2,6 +2,16 @@ package software.ulpgc.edp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Dictionary<String, Integer> d = new Dictionary<>();
+        for (int i = 0; i < 500; i++) {
+            d.put("P" + (i + 1), i);
+        }
+        int counter = 0;
+        for (Dictionary.Entry<String, Integer> entry : d) {
+            System.out.println(entry);
+            counter++;
+        }
+        System.out.println(counter);
     }
+
 }
