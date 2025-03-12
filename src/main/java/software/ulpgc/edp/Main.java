1939764d80520b2c4ea0217817cd9275c75226a6
Vit <vit@email.com>
2025-03-12 15:15:14 +0000

    Comment

--- src/main/java/software/ulpgc/edp/Main.java
+++ src/main/java/software/ulpgc/edp/Main.java
@@ -2,16 +2,13 @@ package software.ulpgc.edp;
 
 public class Main {
     public static void main(String[] args) {
-        Dictionary<String, Integer> d = new Dictionary<>();
-        for (int i = 0; i < 500; i++) {
-            d.put("P" + (i + 1), i);
-        }
-        int counter = 0;
-        for (Dictionary.Entry<String, Integer> entry : d) {
-            System.out.println(entry);
-            counter++;
-        }
-        System.out.println(counter);
+
+        System.out.println("a".hashCode() & 7);
+        System.out.println("b".hashCode() & 7);
+        System.out.println("c".hashCode() & 7);
+        System.out.println("d".hashCode() & 7);
+        System.out.println("e".hashCode() & 7);
     }
+
 
 }
